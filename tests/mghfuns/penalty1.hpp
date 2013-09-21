3bce972b3738e2f17844ab91632c1f0201cae280
Philippe Tillet <Phil.Tillet@gmail.com>
2013-09-21 23:09:22 +0800

    Added Penalty Functions.

---
+++ tests/mghfuns/penalty1.hpp
@@ -0,0 +1,57 @@
+#ifndef FMINCL_PENALTY1_HPP_
+#define FMINCL_PENALTY1_HPP_
+
+#include <cmath>
+#include <vector>
+#include "sum_square.hpp"
+
+using namespace std;
+#define RES(i,j) base_type::get(res,i,j)
+
+template<class BackendType>
+class penalty1 : public sum_square<BackendType>{
+    typedef typename BackendType::VectorType VectorType;
+    typedef double ScalarType;
+    typedef sum_square<BackendType> base_type;
+    static const double a;
+    using base_type::N_;
+    using base_type::M_;
+    using base_type::global_minimum_;
+public:
+    penalty1(std::size_t n) : base_type("Penalty 1",n+1,n,0){
+        if(n==4)
+            global_minimum_ = 2.24997e-5;
+        else if(n==10)
+            global_minimum_ = 7.08765e-5;
+        else
+            throw "Not allowed dimensionality";
+    }
+    void init(VectorType & X) const
+    {
+        for(std::size_t n = 0 ; n < N_ ; ++n)
+            X[n] = n;
+    }
+    void fill_dym_dxn(VectorType const & V, ScalarType * res) const
+    {
+        for(std::size_t m = 0 ; m < N_ ; ++m)
+            RES(m,m) = sqrt(a);
+        for(std::size_t n = 0 ; n < N_ ; ++n)
+            RES(N_,n) = 2*V[n];
+
+
+    }
+    void fill_ym(VectorType const & V, ScalarType * res) const
+    {
+        for(std::size_t m = 0 ; m < N_ ; ++m){
+            res[m] = sqrt(a)*(V[m]-1);
+        }
+        ScalarType sum = 0;
+        for(std::size_t n = 0 ; n < N_ ; ++n)
+            sum+=V[n]*V[n];
+        res[N_] = sum - 1.4;
+    }
+};
+
+template<class BackendType>
+const double penalty1<BackendType>::a = 1e-5;
+#endif
---
+++ tests/mghfuns/penalty2.hpp
@@ -0,0 +1,67 @@
+#ifndef FMINCL_PENALTY2_HPP_
+#define FMINCL_PENALTY2_HPP_
+
+#include <cmath>
+#include <vector>
+#include "sum_square.hpp"
+
+using namespace std;
+#define RES(i,j) base_type::get(res,i,j)
+
+template<class BackendType>
+class penalty2 : public sum_square<BackendType>{
+    typedef typename BackendType::VectorType VectorType;
+    typedef double ScalarType;
+    typedef sum_square<BackendType> base_type;
+    static const double a;
+    using base_type::N_;
+    using base_type::M_;
+public:
+    penalty2(std::size_t n) : base_type("Penalty 2",2*n,n,0){
+        if(n==4)
+            base_type::global_minimum_ = 9.37629e-6;
+        else if(n==10)
+            base_type::global_minimum_ = 2.93660e-6;
+        else
+            throw "Not allowed dimensionality";
+    }
+    void init(VectorType & X) const
+    {
+        for(std::size_t n = 0 ; n < N_ ; ++n)
+            X[n] = 0.5;
+    }
+    void fill_dym_dxn(VectorType const & V, ScalarType * res) const
+    {
+        RES(0,0) = 1;
+        for(std::size_t i = 1 ; i < N_ ; ++i){
+            RES(i,i) = sqrt(a)*0.1*exp(0.1*V[i]);
+            RES(i,i-1) = sqrt(a)*0.1*exp(0.1*V[i-1]);
+        }
+        for(std::size_t i = 0 ; i < N_ ; ++i){
+            RES(i+N_,i) = sqrt(a)*0.1*exp(0.1*V[i]);
+        }
+        for(std::size_t i = 0 ; i < N_ ; ++i)
+            RES(M_-1,i)=2*(N_ - i)*V[i];
+
+    }
+    void fill_ym(VectorType const & V, ScalarType * res) const
+    {
+        res[0] = V[0] - 0.2;
+        for(std::size_t i = 1 ; i < N_ ; ++i){
+            ScalarType y = exp(0.1*(i+1)) + exp(0.1*i);
+            res[i] = sqrt(a)*(exp(0.1*V[i]) + exp(0.1*V[i-1])) - y;
+        }
+        for(std::size_t i = 0 ; i < N_ ; ++i){
+            res[N_+i] = sqrt(a)*(exp(0.1*V[i]) - exp(-0.1));
+        }
+        ScalarType sum = 0;
+        for(std::size_t i = 0 ; i < N_ ; ++i)
+            sum+=(N_ - i)*V[i]*V[i];
+        res[M_-1] = sum - 1;
+
+    }
+};
+
+template<class BackendType>
+const double penalty2<BackendType>::a = 1e-5;
+#endif
